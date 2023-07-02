848478c6db4cddfb999fdaaa3f39f665fa4de471
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-07-02 23:15:17 +0530

    Time: 1 ms (66.59%) | Memory: 44.2 MB (49.41%) - LeetSync

--- n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,15 +1,38 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-       int n = nums.length/2;
-       Arrays.sort(nums);
-       int count = 1;
-       for(int i =0;i<2*n-1;i++){
-           if(nums[i]==nums[i+1]){
-               count++;
-               if(count==n)
-               return nums[i];
-           }
-       }
-       return 1;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                if(i != j && nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int repeatedNTimes(int[] nums) {
+//        int n = nums.length/2;
+//        Arrays.sort(nums);
+//        int count = 1;
+//        for(int i =0;i<2*n-1;i++){
+//            if(nums[i]==nums[i+1]){
+//                count++;
+//                if(count==n)
+//                return nums[i];
+//            }
+//        }
+//        return 1;
+//     }
+// }
