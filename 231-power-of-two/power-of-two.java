a1349e26dded41d7eebaad138e8f860048578cbe
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-07-17 22:17:15 +0530

    Time: 1 ms (100.00%) | Memory: 40 MB (35.24%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -3,22 +3,14 @@ class Solution {
         if(n==1){
             return true;
         }
-        if(n%2!=0)
+        if(n%2!=0 || n<1)
         return false;
-        if(n%2==0){
-            // for(int i=1;i<=n/2;i++){
-            //     if(Math.pow(2,i)!=n)
-            //     return true; 
-            // }
+        if(n%2==0){       
             while(n>2){
                 n = n/2;
                 if(n%2!=0)
                 return false;
-
             }
-            if(n<2)
-            return false;
-
         }
         return true;
     }
