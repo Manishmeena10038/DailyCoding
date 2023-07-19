4878478313114b7c3923fe3c029da5dcea0d6485
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-07-19 22:16:28 +0530

    Time: 1 ms (71.52%) | Memory: 42.3 MB (7.77%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,16 @@
 class Solution {
-  public List<List<Integer>> subsets(int[] nums) {
-    List<List<Integer>> ans = new ArrayList<>();
-    dfs(nums, 0, new ArrayList<>(), ans);
-    return ans;
-  }
-
-  private void dfs(int[] nums, int s, List<Integer> path, List<List<Integer>> ans) {
-    ans.add(new ArrayList<>(path));
-
-    for (int i = s; i < nums.length; ++i) {
-      path.add(nums[i]);
-      dfs(nums, i + 1, path, ans);
-      path.remove(path.size() - 1);
+    public List<List<Integer>> subsets(int[] nums) {
+         List<List<Integer>> subset = new ArrayList<>();
+         subset.add(new ArrayList<>());
+         for(int i = 0; i < nums.length; i++) {
+             int size = subset.size();
+             for(int j = 0; j < size; j++) {
+                 List<Integer> list = new ArrayList<>(subset.get(j));
+                 list.add(nums[i]);
+                 subset.add(list);
+             }
+         }
+         return subset;
+      
     }
-  }
 }
