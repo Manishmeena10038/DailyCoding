0dc0a31f08b12da374593f6dd2e9a711ad0928b5
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-07-23 01:04:55 +0530

    Time: 1 ms (74.63%) | Memory: 39.9 MB (30.31%) - LeetSync

---
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findDelayedArrivalTime(int a, int d) {
+        if(a+d ==24)
+        return 0;
+        if((a+d)>24)
+        return (a+d)-24;
+        return a+d;
+    }
+}
