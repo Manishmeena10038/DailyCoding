8eca0cccc3fdc2cb665e557a87fab3d7c2216ad3
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-07-22 21:46:36 +0530

    Time: 3 ms (12.56%) | Memory: 41.8 MB (99.70%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -3,11 +3,10 @@ class Solution {
         int n = prices.length;
         int sum = 0;
         for(int i =1;i<n;i++){
-            if(prices[i-1]<prices[i]){
+            if(prices[i-1]<prices[i])
                 sum+= prices[i]-prices[i-1];
-
-            }
         }
+        System.gc();
         return sum;
     }
 }
