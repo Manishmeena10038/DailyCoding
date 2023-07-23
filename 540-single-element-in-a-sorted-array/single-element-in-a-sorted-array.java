179a0ee6217c70d71559dc9d81301468d1c74b6c
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-07-23 19:43:14 +0530

    Time: 1 ms (26.14%) | Memory: 50.2 MB (39.22%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,24 +1,53 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        
-        int num =0;
-        if(nums.length==1)
-        num = nums[0];
-        // if(nums.length==3){
-        //     if(nums[0]==nums[1]){
-        //         num = nums[2];
-        //     }
-        //     else
-        //     num = nums[0];
-        // }
-        for(int i=0;i<nums.length-1;i+=2){
-            if(nums[i]!=nums[i+1]){
-            num = nums[i];
-            break;
-            }
-            else
-            num = nums[nums.length-1];
+        if(nums.length==1){
+            return nums[0];
         }
-        return num;
+        int i=0;
+        while(i<nums.length-1){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+            i+=2;
+        }
+        return nums[nums.length-1];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+        
+//         int num =0;
+//         if(nums.length==1)
+//         num = nums[0];
+//         // if(nums.length==3){
+//         //     if(nums[0]==nums[1]){
+//         //         num = nums[2];
+//         //     }
+//         //     else
+//         //     num = nums[0];
+//         // }
+//         for(int i=0;i<nums.length-1;i+=2){
+//             if(nums[i]!=nums[i+1]){
+//             num = nums[i];
+//             break;
+//             }
+//             else
+//             num = nums[nums.length-1];
+//         }
+//         return num;
+//     }
+// }
