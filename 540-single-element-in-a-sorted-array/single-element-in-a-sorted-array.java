2969a97b3d226abc1f156513199f0b78d07c5c7f
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-07-23 19:34:20 +0530

    Time: 1 ms (26.14%) | Memory: 50.3 MB (27.63%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -4,13 +4,13 @@ class Solution {
         int num =0;
         if(nums.length==1)
         num = nums[0];
-        if(nums.length==3){
-            if(nums[0]==nums[1]){
-                num = nums[2];
-            }
-            else
-            num = nums[0];
-        }
+        // if(nums.length==3){
+        //     if(nums[0]==nums[1]){
+        //         num = nums[2];
+        //     }
+        //     else
+        //     num = nums[0];
+        // }
         for(int i=0;i<nums.length-1;i+=2){
             if(nums[i]!=nums[i+1]){
             num = nums[i];
