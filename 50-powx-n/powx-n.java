5da8af4072b1a34e66068f46270fce9ed9238ea2
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-07-24 23:48:15 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (29.42%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,47 @@
+class Solution {
+    public double myPow(double x, int n) {
+        
+
+        // return pow;
+        double pow = Math.pow(x,n);
+        return pow;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// double pow = x; 
+        // int i = 1;
+        // if(x ==0){
+        //     return 0;
+        // }
+        // if(n==0||x==1){
+        //     return 1;
+        // }
+        // if(n ==1){
+        //     return x;
+        // }
+       
+        // if(n>0){       
+        // while(i<n){
+        //     pow = x*pow;
+        //     i++;
+            
+        // }
+        // // return pow;
+        // }
+        // else{
+        //     while(n<-1){
+        //         pow = x*pow;
+        //         n++;
+        //     }
+        //     return 1/pow;
+        // }
