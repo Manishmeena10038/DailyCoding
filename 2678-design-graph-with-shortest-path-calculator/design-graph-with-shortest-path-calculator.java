f8f563e76128c768188b75b368c4a8d2a99e62dd
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-11-11 20:34:13 +0530

    Time: 102 ms (72.22%) | Memory: 55.3 MB (6.48%) - LeetSync

---
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.java
@@ -0,0 +1,53 @@
+class Graph {
+  public Graph(int n, int[][] edges) {
+    graph = new List[n];
+    for (int i = 0; i < n; ++i)
+      graph[i] = new ArrayList<>();
+    for (int[] edge : edges)
+      addEdge(edge);
+  }
+
+  public void addEdge(int[] edge) {
+    final int u = edge[0];
+    final int v = edge[1];
+    final int w = edge[2];
+    graph[u].add(new Pair<>(v, w));
+  }
+
+  public int shortestPath(int node1, int node2) {
+    int[] dist = new int[graph.length];
+    Arrays.fill(dist, Integer.MAX_VALUE);
+    // (d, u)
+    Queue<Pair<Integer, Integer>> minHeap = new PriorityQueue<>(Comparator.comparing(Pair::getKey));
+
+    dist[node1] = 0;
+    minHeap.offer(new Pair<>(dist[node1], node1));
+
+    while (!minHeap.isEmpty()) {
+      final int d = minHeap.peek().getKey();
+      final int u = minHeap.poll().getValue();
+      if (u == node2)
+        return d;
+      for (Pair<Integer, Integer> pair : graph[u]) {
+        final int v = pair.getKey();
+        final int w = pair.getValue();
+        if (d + w < dist[v]) {
+          dist[v] = d + w;
+          minHeap.offer(new Pair<>(dist[v], v));
+        }
+      }
+    }
+
+    return -1;
+  }
+
+  private List<Pair<Integer, Integer>>[] graph;
+}
+
+
+/**
+ * Your Graph object will be instantiated and called as such:
+ * Graph obj = new Graph(n, edges);
+ * obj.addEdge(edge);
+ * int param_2 = obj.shortestPath(node1,node2);
+ */
