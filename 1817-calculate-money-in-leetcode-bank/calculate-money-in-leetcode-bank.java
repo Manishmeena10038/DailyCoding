bc7acd40635527484700eedfad9c92633c75439b
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2023-12-06 22:09:32 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (19.76%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,6 +1,41 @@
 class Solution {
     public int totalMoney(int n) {
-        int a = n / 7, b = n % 7;
-        return (28 + 28 + 7 * (a - 1)) * a / 2 + (a * 2 + b + 1) * b / 2;
+        int sum = 0;
+        int rnds = 28;
+        
+        if (n >= 7) {
+            int rnd = n / 7;
+            int rem = n % 7;
+
+            for (int i = 0; i < rnd; i++) {
+                sum += rnds + 7 * i;
+            }
+
+            for (int k = 1; k <= rem; k++) {
+                sum += rnd + k;
+            }
+        } else {
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 }
+
+
+// int a = 1;
+//         int total = 0;
+//         int temp = 0;
+//         int count =1;
+//         for(int i = a;n>0;i++){
+//             temp +=i;
+//             n--;
+//             if(count%7==0){
+//                 a++;
+//                 i=a-1;
+//             }
+//             count++;
+//         }
+//         return temp;
