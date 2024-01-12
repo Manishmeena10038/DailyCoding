cb7ea535e154bfeadbe380cb70cec0653ec31638
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-01-12 22:39:47 +0530

    Time: 8 ms (28.00%) | Memory: 41.5 MB (38.94%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int count=0;
-        int n = s.length();
-        for(int i=0;i<n/2;i++){
-            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
-                count++;
+        int l = s.length();
+        int half = l/2;
+        int count = 0;
+         
+        s = s.toLowerCase();
+        for(int i=0;i<l;i++){
+        if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+            if(i<half) count++;
+            else count--;
             }
-            if(s.charAt(n-i-1)=='a'||s.charAt(n-i-1)=='e'||s.charAt(n-i-1)=='i'||s.charAt(n-i-1)=='o'||s.charAt(n-i-1)=='u'||s.charAt(n-i-1)=='A'||s.charAt(n-i-1)=='E'||s.charAt(n-i-1)=='I'||s.charAt(n-i-1)=='O'||s.charAt(n-i-1)=='U'){
-                count--;
-            }
-
         }
-        if(count==0)
-        return true;
-        return false;
+        return count==0;
+
     }
 }
