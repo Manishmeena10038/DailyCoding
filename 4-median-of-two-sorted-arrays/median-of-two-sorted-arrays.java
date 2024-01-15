1d4549d8b18e1b96b7a0e3867dc0668fbfb7478e
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-01-15 21:25:14 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (41.40%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,22 +1,47 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int c=nums1.length+nums2.length;
-        int d[]=new int[c];
-        for(int i=0;i<nums1.length;i++){
-            d[i]=nums1[i];
+        int n = nums1.length;
+        int m = nums2.length;
+        int tot = m+n;
+        int[] nums = merge(nums1,n,nums2,m);
+        double result =0;
+        
+        if(tot%2==1) {int mid = tot/2;
+        result = nums[mid];
         }
 
-        for(int i=nums1.length;i<c;i++){
-            d[i]=nums2[i-nums1.length];
+        if(tot%2==0) {int mid1=tot/2,mid2=(tot/2)-1;
+        result = (double)(nums[mid1]+nums[mid2])/2;
         }
-        Arrays.sort(d);
-        int mid=(0+d.length-1)/2;
-        if(d.length%2!=0){
-         return (double)d[mid];
-        }
-        else{
-           return (double)(d[mid]+d[mid+1])/2;
-        }
-        
+        return result;
+
+
     }
+    static int[] merge(int[] nums1, int n, int[] nums2, int m){
+
+        int low=0;
+        int high=0;
+        int[] nums = new int[m+n];
+        int j=0;
+        while(low<n && high<m){
+            if(nums1[low]<=nums2[high]){
+                nums[j++]=nums1[low];
+                low++;
+            }
+            else{
+                nums[j++] = nums2[high];
+                high++;
+            }
+        }
+        while(low<n){
+            nums[j++]=nums1[low];
+                low++;
+        }
+        while(high<m){
+            nums[j++] = nums2[high];
+                high++;
+        }
+        return nums;
+
     }
+}
