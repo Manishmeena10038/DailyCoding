6d4f0457508c909b18b9d5104ca169f716ae965d
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-01-31 21:43:54 +0530

    Time: 1 ms (69.90%) | Memory: 41.7 MB (47.19%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,21 +1,35 @@
+// class Solution {
+//     public int numJewelsInStones(String jewels, String stones) {
+//         int[] ar = new int[52];
+//         int cnt = 0;
+//         for (char c : jewels.toCharArray()) {
+//             if (Character.isLowerCase(c)) {
+//                 ar[c - 'a'] = 1;
+//             } else if (Character.isUpperCase(c)) {
+//                 ar[c - 'A' + 26] = 1;
+//             }
+//         }
+//         for (char c : stones.toCharArray()) {
+//             if (Character.isLowerCase(c) && ar[c - 'a'] == 1) {
+//                 cnt++;
+//             } else if (Character.isUpperCase(c) && ar[c - 'A' + 26] == 1) {
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int[] ar = new int[52];
-        int cnt = 0;
-        for (char c : jewels.toCharArray()) {
-            if (Character.isLowerCase(c)) {
-                ar[c - 'a'] = 1;
-            } else if (Character.isUpperCase(c)) {
-                ar[c - 'A' + 26] = 1;
-            }
+        int result=0;
+        for (int i=0; i<jewels.length();i++){
+            for (int j=0;j<stones.length();j++){
+                if(jewels.charAt(i)==stones.charAt(j)){
+                    result++;
+                }
         }
-        for (char c : stones.toCharArray()) {
-            if (Character.isLowerCase(c) && ar[c - 'a'] == 1) {
-                cnt++;
-            } else if (Character.isUpperCase(c) && ar[c - 'A' + 26] == 1) {
-                cnt++;
-            }
         }
-        return cnt;
+        return result;
     }
 }
