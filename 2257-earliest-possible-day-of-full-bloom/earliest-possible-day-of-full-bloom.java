452e2e2d0bced28c91b0b54d772c389dcbfbb442
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-02-02 23:55:06 +0530

    Time: 60 ms (55.38%) | Memory: 62.1 MB (6.15%) - LeetSync

--- 2257-earliest-possible-day-of-full-bloom/earliest-possible-day-of-full-bloom.java
+++ 2257-earliest-possible-day-of-full-bloom/earliest-possible-day-of-full-bloom.java
@@ -8,15 +8,12 @@ class Solution {
             arr[i][1] = plantTime[i];
         }
         Arrays.sort(arr, (a,b) -> b[0]-a[0]);
-        
          int curr = 0;
          int total = 0;
-
          for(int i=0;i<n;i++){
              total = Math.max(total,curr+arr[i][0]+arr[i][1]);
              curr = curr + arr[i][1];
          }
-         return total;
-        
+         return total;   
     }
 }
