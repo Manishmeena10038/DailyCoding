d7900cda8600cc4b4ef5cd1dd5a0ef659fc15d6b
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-03-14 09:42:46 +0530

    Time: 1 ms (35.54%) | Memory: 42.3 MB (28.85%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,27 +1,51 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int low =0,high=0;
-        ArrayList<Integer>  ar = new ArrayList<>();
-        while(low<m&&high<n){
-            if(nums1[low]<nums2[high]){
-                ar.add(nums1[low]);
-                low++;
+        int a1=m-1;
+        int a2=0;
+        // if(m)
+        // int i=0;
+        // if(n!=0){
+
+        
+        // while(a1<m){
+        //     if(nums1[a1]<=nums2[a2]){
+               
+        //         a1++;
+        //     }
+        //     else{
+        //         int temp = nums1[a1++];
+        //         nums1[i]=nums2[a2];
+        //         nums2[a2]=temp;
+                
+        //     }
+        //     i++;
+        // }
+        // while(a2<n){
+        //     nums1[i++]=nums2[a2++];
+        // }
+        // }
+        if(m!=0&&n!=0)
+        while(a1>=0&&a2>=0){
+            if(nums1[a1]>=nums2[a2]){
+
+            
+            int temp = nums1[a1];
+            nums1[a1] = nums2[a2];
+            nums2[a2] = temp;
+            a1--;
+            a2--;
             }
-            else{
-                ar.add(nums2[high]);
-                high++;
-            }
+            else
+            a2--;
         }
-        while(low<m){
-            ar.add(nums1[low]);
-                low++;
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        a2=0;
+        while(a2<n){
+            nums1[a2+m]=nums2[a2];
+            a2++;
         }
-        while(high<n){
-             ar.add(nums2[high]);
-                high++;
-        }
-        for(int i=0;i<ar.size();i++){
-            nums1[i] = ar.get(i);
-        }
+        Arrays.sort(nums1);
+
     }
 }
