5b71d9a3a107d8c0cd6b4a15c5c83f319e2d12db
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-03-14 09:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (43.91%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int a1=m-1;
-        int a2=0;
+        // int a1=m-1;
+        // int a2=0;
         // if(m)
         // int i=0;
         // if(n!=0){
@@ -24,28 +24,63 @@ class Solution {
         //     nums1[i++]=nums2[a2++];
         // }
         // }
-        if(m!=0&&n!=0)
-        while(a1>=0&&a2>=0){
-            if(nums1[a1]>=nums2[a2]){
+
+
+
+
+        //  if(m!=0&&n!=0)
+        // while(a1>=0&&a2>=0){
+        //     if(nums1[a1]>=nums2[a2]){
 
             
-            int temp = nums1[a1];
-            nums1[a1] = nums2[a2];
-            nums2[a2] = temp;
-            a1--;
-            a2--;
-            }
-            else
-            a2--;
-        }
+        //     int temp = nums1[a1];
+        //     nums1[a1] = nums2[a2];
+        //     nums2[a2] = temp;
+        //     a1--;
+        //     a2--;
+        //     }
+        //     else
+        //     a2--;
+        // }
+        // // Arrays.sort(nums1);
+        // // Arrays.sort(nums2);
+        // a2=0;
+        // while(a2<n){
+        //     nums1[a2+m]=nums2[a2];
+        //     a2++;
+        // }
         // Arrays.sort(nums1);
-        // Arrays.sort(nums2);
-        a2=0;
-        while(a2<n){
-            nums1[a2+m]=nums2[a2];
-            a2++;
-        }
-        Arrays.sort(nums1);
 
+
+
+
+     int i = m-1;
+     int j=n-1;
+     int k = n+m-1;
+     while(j>=0){
+        if(i>=0&&nums1[i]>nums2[j]){
+            nums1[k--]=nums1[i--];
+        }
+        else{
+            nums1[k--]=nums2[j];
+            j--;
+        }
+     }
     }
 }
+
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//     int i=m-1,j=n-1,k=m+n-1;
+//      while(j>=0){
+//         if(i>=0&&nums1[i]>nums2[j]){
+//             nums1[k--]=nums1[i];
+//             i--;
+//         }
+//         else{
+//            nums1[k--]=nums2[j];
+//             j--;
+//         }
+//      }
+// }
+// }
