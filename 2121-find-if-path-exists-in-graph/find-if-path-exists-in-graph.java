dd155293be4b26081f1ddafefbfed4c2a156a782
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-04-21 23:10:57 +0530

    Time: 3 ms (99.62%) | Memory: 109 MB (96.08%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if(edges.length == 0) return true;
+        boolean[] visited = new boolean[n];
+        boolean flag = true;
+        visited[source] = true;
+    while(flag){
+        flag = false;
+        for(int[] edge : edges){
+            if(visited[edge[0]] != visited[edge[1]]){
+                visited[edge[0]] = true;
+                visited[edge[1]] = true;
+                flag = true;
+            }
+            if(visited[destination]) return true;
+        }
+    }
+    return false;
+    }
+}
