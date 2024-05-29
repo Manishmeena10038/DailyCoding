5a92a8262d226c601b68d714318c31e1e8dd2bf5
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-05-29 18:18:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (43.61%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,26 +1,41 @@
+// class Solution {
+//     public int numSteps(String s) {
+//         int carry =0;
+//         int cnt = 0;
+//         int l = s.length()-1;
+//         while(l>0){
+//             if(Character.getNumericValue(s.charAt(l))+carry==0){
+//                 carry =0;
+//                 cnt++;
+//             }
+//             else if(Character.getNumericValue(s.charAt(l))+carry==2){
+//                 carry = 1;
+//                 cnt++;
+//             }
+//             else{
+//                 carry = 1;
+//                 cnt+=2;
+//             }
+//             l--;
+//         }
+//         if(carry==1){
+//             cnt++;
+//         }
+//         return cnt;
+//     }
+// }
+
 class Solution {
     public int numSteps(String s) {
-        int carry =0;
-        int cnt = 0;
-        int l = s.length()-1;
-        while(l>0){
-            if(Character.getNumericValue(s.charAt(l))+carry==0){
-                carry =0;
-                cnt++;
+        int ans =0;
+        int carry=0;
+        for(int i=s.length()-1;i>0;i--){
+            ans++;
+            if(s.charAt(i)-'0'+carry==1){
+                carry =1;
+                ans++;
             }
-            else if(Character.getNumericValue(s.charAt(l))+carry==2){
-                carry = 1;
-                cnt++;
-            }
-            else{
-                carry = 1;
-                cnt+=2;
-            }
-            l--;
         }
-        if(carry==1){
-            cnt++;
-        }
-        return cnt;
+        return ans+carry;
     }
 }
