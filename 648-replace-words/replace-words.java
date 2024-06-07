a37b2e1d2411b6386dd63c389056f8a8f7c1ba72
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-06-07 22:40:05 +0530

    Time: 695 ms (12.74%) | Memory: 55.4 MB (45.90%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,53 +1,25 @@
 class Solution {
-    Trie root;
-    public String replaceWords(List<String> dictionary, String sentence) {
-        root = new Trie();
-        for(String word : dictionary){
-            insert(word);
-        }
-        StringBuilder result = new StringBuilder();
-        String []  input = sentence.split(" ");
-        for(String i : input){
-            result.append(search(i));
-            result.append(" ");
-        }
-        return result.toString().trim();
-    }
-    public String search(String word){
-        Trie node = root;
-        int j = 0;
-        for(char c : word.toCharArray()){
-            int i = c - 'a';
-            j++;
-            if(node.children[i] == null){
-                return word;
-            }else if(node.children[i].isEnd){
-                return word.substring(0, j);
-            }else{
-                node = node.children[i];
-            }
-            
-        }
-        return word;
+    public String replaceWords(List<String> dict, String sentence) {
+        Set<String> s = new HashSet<>(dict);
+        StringBuilder res = new StringBuilder();
+        List<String> temp = new ArrayList<>(
+            Arrays.asList(sentence.split(" "))
+        );
+        
+        for (String t : temp) {
+            int i = 0;
+            while (i <= t.length()) {
+                String curr = t.substring(0, i++);
+                if (s.contains(curr)) {
+                    res.append(curr).append(" ");
+                    break;
+                }
 
-    }
-    public void insert(String word){
-        Trie node = root;
-        for(char c: word.toCharArray()){
-            int i = c - 'a';
-            if(node.children[i] == null){
-                node.children[i] = new Trie();
+                if (i == t.length() + 1) res.append(t).append(" ");
             }
-            node = node.children[i];
         }
-        node.isEnd = true;
+        
+        res.deleteCharAt(res.length() - 1); 
+        return res.toString();
     }
 }
-class Trie{
-    Trie [] children;
-    boolean isEnd;
-    public Trie(){
-        children = new Trie[26];
-        isEnd =false;
-    }
-} 
