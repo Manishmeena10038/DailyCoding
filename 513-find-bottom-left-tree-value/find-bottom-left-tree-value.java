9ebc6ac63e901acd3fcd32a7802c69d171d1c2ac
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-06-11 12:07:00 +0530

    Time: 1 ms (71.85%) | Memory: 44.9 MB (20.99%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -15,22 +15,45 @@
  */
 public class Solution {
     public int findBottomLeftValue(TreeNode root) {
-        int last = 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        List<Integer> list = new ArrayList<>();
+        rightSideView(root,list);
+        return list.get(list.size()-1);
 
-        while (!q.isEmpty()) {
-            int count = q.size();
-            for (int i = 0; i < count; i++) {
-                TreeNode curr = q.poll();
-                if (i == 0)
-                    last = curr.val;  // last leftMost val
-                if (curr.left != null)
-                    q.add(curr.left);
-                if (curr.right != null)
-                    q.add(curr.right);
-            }
+        // int last = 0;
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.add(root);
+
+        // while (!q.isEmpty()) {
+        //     int count = q.size();
+        //     for (int i = 0; i < count; i++) {
+        //         TreeNode curr = q.poll();
+        //         if (i == 0)
+        //             last = curr.val;  // last leftMost val
+        //         if (curr.left != null)
+        //             q.add(curr.left);
+        //         if (curr.right != null)
+        //             q.add(curr.right);
+        //     }
+        // }
+        // return last;
+
+    }
+    public List<Integer> rightSideView(TreeNode root,List<Integer> list ) {
+        
+        Queue<Integer> q =new LinkedList<Integer>();
+        right(list,0,root,q);
+        return list;
+
+    }
+    public static void right(List<Integer> list,int level, TreeNode root,Queue<Integer> q){
+        if(root==null) return;
+        if(level==q.size()){
+            q.add(root.val);
+            list.add(root.val);
         }
-        return last;
+        
+        right(list,level+1,root.left, q);
+        right(list,level+1,root.right, q);
+        
     }
 }
