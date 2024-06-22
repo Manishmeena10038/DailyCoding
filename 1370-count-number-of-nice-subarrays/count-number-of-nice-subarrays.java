05b2996df1da437077a3fbc3a25034de10620472
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-06-22 14:18:35 +0530

    Time: 41 ms (16.26%) | Memory: 55.9 MB (5.81%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -3,18 +3,11 @@ class Solution {
         int cnt =0;
         int ans =0;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }
-            else{
-                nums[i]=1; 
-            }
-        }
+      
         HashMap<Integer,Integer> map =  new HashMap<>();
         map.put(0,1);
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==1) cnt++;
+            if((nums[i]%2!=0)) cnt++;
             int dif = cnt-k;
             ans+=map.getOrDefault(dif,0);
             map.put(cnt,map.getOrDefault(cnt,0)+1);
