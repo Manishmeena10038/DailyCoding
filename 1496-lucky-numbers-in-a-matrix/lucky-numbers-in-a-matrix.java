77f89909c27e8b4572f4a90143e4996c92f02f4b
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-07-19 18:57:19 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (19.38%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -1,24 +1,76 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
-       HashSet<Integer> set = new HashSet<>();
-       ArrayList<Integer> ans = new ArrayList<>();
+       ArrayList<Integer> arr = new ArrayList<>();
+       for(int i=0;i<matrix.length;i++){
+        int col = findmin(matrix,i);
+        int maxcol = matrix[i][col];
+        if(maxcol(matrix,maxcol,col)){
+        arr.add(maxcol);
+       }
+       }
 
+       
+       return arr;
+    }
+    public static int findmin(int[][] matrix,int i){
+        int min = matrix[i][0];
+        int col = 0;
+        for(int j=0;j<matrix[i].length;j++){
+            if(min>matrix[i][j]){
+                min = matrix[i][j];
+                col = j;
+            }
+        }
+        return col;
+    }
+    public static boolean maxcol(int[][] matrix , int max, int col ){
         for(int i=0;i<matrix.length;i++){
-            int minr = matrix[i][0];
-            for(int j=0;j<matrix[0].length;j++){
-                minr = Math.min(minr,matrix[i][j]);
-            }
-            set.add(minr);
-        }
-        for(int i=0;i<matrix[0].length;i++){
-            int maxc = matrix[0][i];
-            for(int j=0;j<matrix.length;j++){
-                maxc = Math.max(maxc,matrix[j][i]);
-            }
-            if(set.contains(maxc)){
-                ans.add(maxc);
+            if(max<matrix[i][col]){
+                return false;
             }
         }
-        return ans;
+        return true;
     }
 }
+
+// class Solution {
+//     public List<Integer> luckyNumbers (int[][] matrix) {
+//         List<Integer> lsRes = new ArrayList<>();
+
+//         for(int i = 0; i < matrix.length; i++)
+//         {
+//             int col = findMin(matrix, i);
+
+//             int maxCol = matrix[i][col];
+//             if(maxInCol(matrix, maxCol, col))
+//                 lsRes.add(maxCol);
+//             // for(int r = 0; r < matrix.length; r++)
+//             // {
+//             //     if(matrix[r][col] > maxCol)
+//             //         maxCol = matrix[r][col];
+//             // }
+
+//             // if(minRow == maxCol)
+//             //     lsRes.add(matrix[i][col]);
+//         }
+
+//         return lsRes;
+//     }
+//     private int findMin(int[][] matrix, int row){
+// 		int val= matrix[row][0], col=0;
+// 		for(int i=1;i<matrix[row].length;i++){
+// 			if(matrix[row][i]<val){
+// 				val=matrix[row][i];
+// 				col=i;
+// 			}
+// 		}
+// 		return col;
+// 	}
+//     private boolean maxInCol(int[][] matrix, int val, int col){
+//         for(int i=0;i<matrix.length;i++){
+//             if(matrix[i][col]>val)
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
