b8796a3ffb0bd699fc8a53d27767f1a3fb54e768
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-07-28 16:26:47 +0530

    Time: 88 ms (10.46%) | Memory: 57 MB (16.92%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -46,46 +46,55 @@ class Solution {
         // }
         // return arr;
 
-        divide(arr,0,arr.length-1);
+        // divide(arr,0,arr.length-1);
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        for(int i:arr){
+            q.add(i);
+        }
+        int i=0;
+        while(!q.isEmpty()){
+            arr[i++] = q.poll();
+        }
        
         return arr;
     }
-    public static void divide(int[] arr, int low, int high){
-        if(low>=high){
-            return;
-        }
-        int mid = (low+high)/2;
-        divide(arr,low,mid);
-        divide(arr,mid+1,high);
-        merge(arr,low,mid,high);
+    // public static void divide(int[] arr, int low, int high){
+    //     if(low>=high){
+    //         return;
+    //     }
+    //     int mid = (low+high)/2;
+    //     divide(arr,low,mid);
+    //     divide(arr,mid+1,high);
+    //     merge(arr,low,mid,high);
         
-    }
-    public static void merge(int[] arr, int low ,int mid,int high){
-        int left=low;
-        int right = mid+1;
-        ArrayList<Integer> temp = new ArrayList<>();
+    // }
+    // public static void merge(int[] arr, int low ,int mid,int high){
+    //     int left=low;
+    //     int right = mid+1;
+    //     ArrayList<Integer> temp = new ArrayList<>();
 
-        while(left<=mid&&right<=high){
-            if(arr[left]<=arr[right]){
-                temp.add(arr[left]);
-                left++;
-            }
-            else{
-                temp.add(arr[right]);
-                right++;
-            }
-        }
-        while(left<=mid){
-            temp.add(arr[left]);
-            left++;
-        }
-        while(right<=high){
-            temp.add(arr[right]);
-            right++;
-        }
+    //     while(left<=mid&&right<=high){
+    //         if(arr[left]<=arr[right]){
+    //             temp.add(arr[left]);
+    //             left++;
+    //         }
+    //         else{
+    //             temp.add(arr[right]);
+    //             right++;
+    //         }
+    //     }
+    //     while(left<=mid){
+    //         temp.add(arr[left]);
+    //         left++;
+    //     }
+    //     while(right<=high){
+    //         temp.add(arr[right]);
+    //         right++;
+    //     }
 
-        for(int i=low;i<=high;i++){
-            arr[i] = temp.get(i-low);
-        }
-    }
+    //     for(int i=low;i<=high;i++){
+    //         arr[i] = temp.get(i-low);
+    //     }
+    // }
 }
