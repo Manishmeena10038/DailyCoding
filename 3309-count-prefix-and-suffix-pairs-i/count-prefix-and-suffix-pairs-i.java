9984d478caea32a1dbc1bf7cc4e60e3b3748070d
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-08-03 16:43:23 +0530

    Time: 2 ms (99.53%) | Memory: 42.5 MB (40.71%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
@@ -1,23 +1,37 @@
+// class Solution {
+//     public int countPrefixSuffixPairs(String[] words) {
+//         int cnt =0;
+
+//         for(int i=0;i<words.length;i++){
+//             for(int j=i+1;j<words.length;j++){
+//                 String stri = words[i];
+//                 int n = stri.length();
+
+//                 String strj = words[j];
+//                 int m = strj.length();
+
+//                 if(m>=n){
+//                     String f = strj.substring(0,n);
+//                     String l = strj.substring(m-n,m);
+//                     if(f.equals(stri)&&l.equals(stri))cnt++;
+//                 }
+                
+//             }
+//         } 
+//         return cnt;
+//     }
+// }
+
+
 class Solution {
     public int countPrefixSuffixPairs(String[] words) {
-        int cnt =0;
-
-        for(int i=0;i<words.length;i++){
-            for(int j=i+1;j<words.length;j++){
-                String stri = words[i];
-                int n = stri.length();
-
-                String strj = words[j];
-                int m = strj.length();
-
-                if(m>=n){
-                    String f = strj.substring(0,n);
-                    String l = strj.substring(m-n,m);
-                    if(f.equals(stri)&&l.equals(stri))cnt++;
-                }
-                
+        int res = 0;
+        for(int i = 0; i < words.length; i++){
+            for(int j = i+1; j < words.length; j++){
+                if(words[j].endsWith(words[i]) && words[j].startsWith(words[i]))
+                    res++;
             }
-        } 
-        return cnt;
+        }
+        return res;
     }
 }
