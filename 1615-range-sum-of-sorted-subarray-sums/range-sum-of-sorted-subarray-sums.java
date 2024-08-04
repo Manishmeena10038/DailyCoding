ab79e4007c09c494a5a49b4859ea6d82603a9307
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-08-04 10:06:19 +0530

    Time: 70 ms (47.75%) | Memory: 46.8 MB (39.19%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,19 +1,19 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
         int mod = 1000000007;
-        ArrayList<Integer> arr = new ArrayList<>();
-
+        int[] arr = new int[n*(n+1)/2];
+        int k=0;
         for(int i=0;i<n;i++){
             int sum=0;
             for(int j=i;j<n;j++){
                 sum=(sum+nums[j])%mod;
-                arr.add(sum);
+                arr[k++]= sum;
             }
         }
-        Collections.sort(arr);
+        Arrays.sort(arr);
         int ans = 0;
         for(int i=left;i<=right;i++){
-            ans=(ans+arr.get(i-1))%mod;
+            ans=(ans+arr[i-1])%mod;
         }
         return ans%mod;
     }
