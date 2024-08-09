08f39e74c22e4b70d8b60b5b24cada225c38fea3
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-08-09 12:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (60.13%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,17 +1,30 @@
 class Solution {
-  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    final int m = obstacleGrid.length;
-    final int n = obstacleGrid[0].length;
-    int[] dp = new int[n];
-    dp[0] = 1;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
 
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        if (obstacleGrid[i][j] == 1)
-          dp[j] = 0;
-        else if (j > 0)
-          dp[j] += dp[j - 1];
+    //     int ans = findways(grid,n,m,0,0);
+    //     return ans;
+    // }
+    // public static int findways(int[][] grid, int n, int m, int curx, int cury){
+    //     if(curx>=n||cury>=m||grid[curx][cury]==1)return 0;
+    //     if(curx==n-1&&cury==m-1)return 1;
 
-    return dp[n - 1];
-  }
+    //     return findways(grid,n,m,curx+1,cury)+findways(grid,n,m,curx,cury+1);
+
+    int dp[] = new int[m];
+    dp[0]=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==1){
+                dp[j]=0;
+            }
+            else if(j>0){
+                dp[j]+=dp[j-1];
+            }
+        }
+       
+    }
+     return dp[m-1];
+    }
 }
