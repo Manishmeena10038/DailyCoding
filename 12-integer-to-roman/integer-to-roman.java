e4d47c04f631c90ab9d57b220ee8adda2f8580b0
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-08-10 09:07:49 +0530

    Time: 13 ms (6.71%) | Memory: 44.7 MB (22.58%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String intToRoman(int num) {
+        String s = itor(num);
+        return s;
+    }
+    public static String itor(int num){
+        String[] k = {"","M","MM","MMM"};
+        String[] hun = {"", "C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] ten = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] one = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        return k[num/1000]+hun[(num%1000)/100]+ten[((num%100))/10]+one[num%10];
+
+    }
+}
