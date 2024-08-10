85520c37377afcbe847864e5c947209b13202fea
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-08-10 09:15:00 +0530

    Time: 3 ms (97.72%) | Memory: 44.5 MB (37.03%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -4,7 +4,14 @@ class Solution {
         String[] hun = {"", "C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
         String[] ten = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
         String[] one = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        return k[num/1000]+hun[(num%1000)/100]+ten[((num%100))/10]+one[num%10];
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(k[num/1000]);
+        sb.append(hun[(num%1000)/100]);
+        sb.append(ten[((num%100))/10]);
+        sb.append(one[num%10]);
+
+        return sb.toString();
     }
     
 }
