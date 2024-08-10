d88ebbae76c1b6cd68f06b5db03347c9b1694dfa
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-08-10 09:46:18 +0530

    Time: 2 ms (63.50%) | Memory: 45.7 MB (75.84%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,46 +2,40 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        int tot = m+n;
-        int[] nums = merge(nums1,n,nums2,m);
-        double result =0;
-        
-        if(tot%2==1) {int mid = tot/2;
-        result = nums[mid];
-        }
 
-        if(tot%2==0) {int mid1=tot/2,mid2=(tot/2)-1;
-        result = (double)(nums[mid1]+nums[mid2])/2;
-        }
-        return result;
+        int nm = n+m;
+        int nums[] = new int[nm];
+
+        int i=n-1;
+        int j = m-1;
+        int k =n+m-1;
 
 
-    }
-    static int[] merge(int[] nums1, int n, int[] nums2, int m){
 
-        int low=0;
-        int high=0;
-        int[] nums = new int[m+n];
-        int j=0;
-        while(low<n && high<m){
-            if(nums1[low]<=nums2[high]){
-                nums[j++]=nums1[low];
-                low++;
+        while(i>=0&&j>=0){
+            if(nums1[i]>nums2[j]){
+                nums[k--] = nums1[i--];
             }
             else{
-                nums[j++] = nums2[high];
-                high++;
+                nums[k--] = nums2[j--];
             }
         }
-        while(low<n){
-            nums[j++]=nums1[low];
-                low++;
+        while(i>=0){
+            nums[k--] = nums1[i--];
         }
-        while(high<m){
-            nums[j++] = nums2[high];
-                high++;
+        while(j>=0){
+            nums[k--] = nums2[j--];
         }
-        return nums;
 
+        double result = 0;
+
+
+        if(nm%2==0){
+            result = (nums[nm/2]+nums[(nm/2)-1])/2.0;
+        }
+        else{
+            result = nums[nm/2];
+        }
+        return result;
     }
 }
