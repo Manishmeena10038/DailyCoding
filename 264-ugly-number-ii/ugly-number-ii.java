a8bc5f08e984111636499dae57d3e6db387f664f
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-08-18 19:17:06 +0530

    Time: 2 ms (91.97%) | Memory: 41.9 MB (41.86%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,19 +1,33 @@
 class Solution {
     public int nthUglyNumber(int n) {
-       int[] factor = {2,3,5};
-       PriorityQueue<Long> q = new PriorityQueue<>();
-       HashSet<Long> set = new HashSet<>();
-       q.offer(1L);
-       set.add(1L);
-       long temp=0;
-       for(int i=1;i<=n;i++){
-        temp = q.poll();
-        for(int j:factor){
-            if(!set.contains(temp*j))
-            q.offer(temp*j);
-            set.add(temp*j);
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        int i2, i3, i5;
+        i2 = i3 = i5 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int n2 = dp[i2] * 2;
+            int n3 = dp[i3] * 3;
+            int n5 = dp[i5] * 5;
+
+            int toInsert = Math.min(n2, Math.min(n3, n5));
+            dp[i] = toInsert;
+
+            // since duplicates might exist so we cant use else if, because there might be a
+            // case when we want to increase both the pointers.
+
+            // example: i2 and i3=6, ab 6 ans mei daal diya hai oh dono pointers ko aage
+            // badhao
+            if (toInsert == n2)
+                i2++;
+            if (toInsert == n3)
+                i3++;
+            if (toInsert == n5)
+                i5++;
         }
-       }
-       return (int)temp;
+        return dp[n];
+
     }
 }
