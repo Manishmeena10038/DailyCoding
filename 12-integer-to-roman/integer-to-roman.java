b2a4f06a038571c5d7330245a5c80988bc77c895
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-09-04 10:36:14 +0530

    Time: 3 ms (97.78%) | Memory: 43.9 MB (98.15%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,10 +1,22 @@
 class Solution {
     public String intToRoman(int num) {
-      String one[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-      String ten[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-      String hun[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-      String th[] = {"","M","MM","MMM"};
+    //   String one[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+    //   String ten[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    //   String hun[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    //   String th[] = {"","M","MM","MMM"};
 
-      return th[num/1000]+hun[(num%1000)/100]+ten[(num%100)/10]+one[(num%10)];
+    //   return th[num/1000]+hun[(num%1000)/100]+ten[(num%100)/10]+one[(num%10)];
+
+    int value[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+    String roman[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+    StringBuilder sb = new StringBuilder();
+
+    for(int i=value.length-1;i>=0;i--){
+        while(num>=value[i]){
+            sb.append(roman[i]);
+            num-=value[i];
+        }
+    }
+    return sb.toString();
     }
 }
