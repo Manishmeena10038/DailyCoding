ff92522012b1cbb099541b28428b74f6d8e11416
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2024-12-12 23:41:22 +0530

    Time: 5 ms (89.83%) | Memory: 42.7 MB (21.79%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long pickGifts(int[] g, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int val : g)
+            maxHeap.add(val);
+
+        for (int i = 0; i < k && maxHeap.peek() > 1; i++) {
+            int x = maxHeap.poll();
+            maxHeap.add((int) Math.sqrt(x));
+        }
+
+        long sum = 0;
+        for (int val : maxHeap)
+            sum += val;
+
+        return sum;
+    }
+}
