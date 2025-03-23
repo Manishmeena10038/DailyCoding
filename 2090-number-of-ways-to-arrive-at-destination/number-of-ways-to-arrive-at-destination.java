a009c2eb71bee300fb48a48b444e99080f22cf29
Manish Meena <127216858+Manishmeena10038@users.noreply.github.com>
2025-03-23 20:33:29 +0530

    Time: 5 ms (99.66%) | Memory: 51 MB (9.40%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int countPaths(int n, int[][] roads) {
+        final long inf = Long.MAX_VALUE / 2;
+        final int mod = (int) 1e9 + 7;
+        long[][] g = new long[n][n];
+        for (var e : g) {
+            Arrays.fill(e, inf);
+        }
+        for (var r : roads) {
+            int u = r[0], v = r[1], t = r[2];
+            g[u][v] = t;
+            g[v][u] = t;
+        }
+        g[0][0] = 0;
+        long[] dist = new long[n];
+        Arrays.fill(dist, inf);
+        dist[0] = 0;
+        long[] f = new long[n];
+        f[0] = 1;
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            int t = -1;
+            for (int j = 0; j < n; j++) {
+                if (!vis[j] && (t == -1 || dist[j] < dist[t])) {
+                    t = j;
+                }
+            }
+            vis[t] = true;
+            for (int j = 0; j < n; j++) {
+                if (j == t) {
+                    continue;
+                }
+                long ne = dist[t] + g[t][j];
+                if (dist[j] > ne) {
+                    dist[j] = ne;
+                    f[j] = f[t];
+                } else if (dist[j] == ne) {
+                    f[j] = (f[j] + f[t]) % mod;
+                }
+            }
+        }
+        return (int) f[n - 1];
+    }
+}
